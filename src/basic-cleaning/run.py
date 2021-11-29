13e0109784861e3cec7ff1ddb06687a4c8fbd6a7
Alex Spiers <advspiers@gmail.com>
2021-11-29 08:51:48 +0000

    Add spatial boundary to basic-cleaning step

--- src/basic-cleaning/run.py
+++ src/basic-cleaning/run.py
@@ -36,6 +36,10 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Ensure data within spatial boundaries
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Create temporary directory
     with tempfile.TemporaryDirectory() as temp_dir:
 
